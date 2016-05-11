4361dd05ac318a40973a24e3536c39effb1106ca
Christoph Uschkrat <christoph@uschkrat.com>
2016-05-11 15:12:23 +0200

    Trigger CI build

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import fs from 'fs';
 
-test.cb('read file', t => {
+test.cb('check script for string', t => {
   fs.readFile('index.js', 'utf-8', (err, data) => {
     if (!err && data.match(/\\u001b\]/)) {
       t.pass();
